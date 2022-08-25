17e06553b72a78ccb1a7ed9718535bc67ae6eba2
rodolformartins <rodolfo.martins@dilettasolutions.com>
2022-08-25 08:13:09 -0300

    insert user model and routes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectToDataBase();
 //require("./modules/fs");
 //require("./modules/http");
 
-//require("./modules/express");
+require("./modules/express");
 
 
 //const person = new Person('Rodolfo');
--- modules/express.js
+++ modules/express.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const UserModel = require('../src/models/user.model');
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/home", (req, res) => {
     res.contentType('application/html');
     res.status(200).send('<h1>hello world!</h1>')
 });
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name: 'Jhon Doe',
-            email: 'john@doe.com'
-        },
-        {
-            name: 'Jane Doe',
-            email: 'jane@doe.com'
-        }
-    ];
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({});
 
-    res.status(200).json(users);
+        res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body)
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
 const port = 8080;
