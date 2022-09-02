5070dfabb37c8f8080d3aee91c3f2fc00f70db67
rodolformartins <rodolfo.martins@dilettasolutions.com>
2022-09-02 08:27:55 -0300

    finish first step

--- modules/express.js
+++ modules/express.js
@@ -5,11 +5,20 @@ const app = express();
 
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
 app.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`);
     console.log(`Content Type: ${req.headers["content-type"]}`);
     console.log(`Date: ${new Date()}`)
     next();
+});
+
+app.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({})
+
+    res.render('index', { users });
 })
 
 app.get("/home", (req, res) => {
